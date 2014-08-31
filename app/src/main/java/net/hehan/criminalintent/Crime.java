57f438aaa4f994e8b4b274d53c29ffac11cd8848
Han He <han@he>
2014-08-31 16:51:50 +0800

    init criminal intent repo

---
+++ app/src/main/java/net/hehan/criminalintent/Crime.java
@@ -0,0 +1,46 @@
+package net.hehan.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by han on 14-8-31.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
